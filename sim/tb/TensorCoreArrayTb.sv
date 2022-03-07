3e056ff35b72d76d3c5a17858b2bde7127081fbb
Tianchu Ji <tianchu.ji@stonybrook.edu>
2022-03-07 11:52:25 -0500

    add tensor core array testbench

---
+++ sim/tb/TensorCoreArrayTb.sv
@@ -0,0 +1,166 @@
+`timescale 1ns/100ps
+
+module TensorCoreArrayTb;
+
+// Parameters
+localparam CHAIN_LEN = 3;
+
+// Ports
+logic          io_matALoad_0_valid;
+logic [319:0]  io_matALoad_0_payload;
+logic          io_matALoad_1_valid;
+logic [319:0]  io_matALoad_1_payload;
+logic          io_matALoad_2_valid;
+logic [319:0]  io_matALoad_2_payload;
+logic          io_matBLoad_0_0_valid;
+logic [319:0]  io_matBLoad_0_0_payload;
+logic          io_matBLoad_0_1_valid;
+logic [319:0]  io_matBLoad_0_1_payload;
+logic          io_matBLoad_0_2_valid;
+logic [319:0]  io_matBLoad_0_2_payload;
+logic          io_matBLoad_1_0_valid;
+logic [319:0]  io_matBLoad_1_0_payload;
+logic          io_matBLoad_1_1_valid;
+logic [319:0]  io_matBLoad_1_1_payload;
+logic          io_matBLoad_1_2_valid;
+logic [319:0]  io_matBLoad_1_2_payload;
+logic          io_calEn;
+logic [7:0]    io_computeIters;
+logic          io_res_valid;
+logic [95:0]   io_res_payload_0_0;
+logic [95:0]   io_res_payload_0_1;
+logic [95:0]   io_res_payload_0_2;
+logic [95:0]   io_res_payload_1_0;
+logic [95:0]   io_res_payload_1_1;
+logic [95:0]   io_res_payload_1_2;
+logic          clk;
+logic          resetn=0;
+
+// Mat A: 9x90, each ceil 8 bitsx10 elems + 1 shared exp 
+logic [319:0] mat_a0 [3*9-1:0];
+logic [319:0] mat_a1 [3*9-1:0];
+logic [319:0] mat_a2 [3*9-1:0];
+// Mat B: 90x18, each ceil 3x (8bits x 10 elems + 1 shared exp)
+logic [319:0] mat_b0 [9*9-1:0];
+logic [319:0] mat_b1 [9*9-1:0];
+// res: 3x9 matrix, each 24 bit
+logic [95:0] res [9*6-1:0];
+
+TensorCoreChainArray dut (.*);
+
+integer i, j, k;
+integer matBRow;
+
+initial begin
+  $readmemh("./tb/MAT_A_FP32_0.mem", mat_a0);
+  $readmemh("./tb/MAT_A_FP32_1.mem", mat_a1);
+  $readmemh("./tb/MAT_A_FP32_2.mem", mat_a2);
+  io_matALoad_0_payload = 320'd0;
+  io_matALoad_1_payload = 320'd0;
+  io_matALoad_2_payload = 320'd0;
+  io_matALoad_0_valid = 0;
+  io_matALoad_1_valid = 0;
+  io_matALoad_2_valid = 0;
+  #21 resetn = 1'b1;
+  
+  @(posedge clk);
+  for (i = 0; i < 3*9; i++) begin
+    @(posedge clk);
+	#1
+    io_matALoad_0_payload = mat_a0[i];
+    io_matALoad_1_payload = mat_a1[i];
+    io_matALoad_2_payload = mat_a2[i];
+    io_matALoad_0_valid = 1;
+    io_matALoad_1_valid = 1;
+    io_matALoad_2_valid = 1;
+  end
+
+  @(posedge clk);
+  #1
+  io_matALoad_0_payload = 320'd0;
+  io_matALoad_1_payload = 320'd0;
+  io_matALoad_2_payload = 320'd0;
+  io_matALoad_0_valid = 0;
+  io_matALoad_1_valid = 0;
+  io_matALoad_2_valid = 0;
+  
+end
+
+initial begin
+  $readmemh("./tb/MAT_B_FP32_0.mem", mat_b0);
+  $readmemh("./tb/MAT_B_FP32_1.mem", mat_b1);
+  io_computeIters = 8'd9;
+  io_matBLoad_0_0_valid = '0;
+  io_matBLoad_0_0_payload = '0;
+  io_matBLoad_0_1_valid = '0;
+  io_matBLoad_0_1_payload = '0;
+  io_matBLoad_0_2_valid = '0;
+  io_matBLoad_0_2_payload = '0;
+  io_matBLoad_1_0_valid = '0;
+  io_matBLoad_1_0_payload = '0;
+  io_matBLoad_1_1_valid = '0;
+  io_matBLoad_1_1_payload = '0;
+  io_matBLoad_1_2_valid = '0;
+  io_matBLoad_1_2_payload = '0;
+
+  for (j=0; j<3*9; j++) begin
+    @(posedge clk);
+	#1
+    {io_matBLoad_0_0_payload, io_matBLoad_0_1_payload, io_matBLoad_0_2_payload} = 
+      {mat_b0[j*3], mat_b0[j*3+1], mat_b0[j*3+2]};
+    {io_matBLoad_1_0_payload, io_matBLoad_1_1_payload, io_matBLoad_1_2_payload} = 
+      {mat_b1[j*3], mat_b1[j*3+1], mat_b1[j*3+2]};
+    io_matBLoad_0_0_valid = 1'b1;
+    io_matBLoad_0_1_valid = 1'b1;
+    io_matBLoad_0_2_valid = 1'b1;
+    io_matBLoad_1_0_valid = 1'b1;
+    io_matBLoad_1_1_valid = 1'b1;
+    io_matBLoad_1_2_valid = 1'b1;
+  end
+
+  @(posedge clk);
+  #1
+  io_matBLoad_0_0_valid = '0;
+  io_matBLoad_0_0_payload = '0;
+  io_matBLoad_0_1_valid = '0;
+  io_matBLoad_0_1_payload = '0;
+  io_matBLoad_0_2_valid = '0;
+  io_matBLoad_0_2_payload = '0;
+  io_matBLoad_1_0_valid = '0;
+  io_matBLoad_1_0_payload = '0;
+  io_matBLoad_1_1_valid = '0;
+  io_matBLoad_1_1_payload = '0;
+  io_matBLoad_1_2_valid = '0;
+  io_matBLoad_1_2_payload = '0;
+end
+
+initial begin
+  for(k=0; k<100; k++) begin
+    @(posedge clk);
+  end
+
+  @(posedge clk);
+  io_calEn = 1'b1;
+
+  @(posedge clk);
+  io_calEn = 1'b0;
+
+  wait(io_res_valid == 1);
+  for(k=0; k<9; k++) begin
+	@(posedge clk);
+    res[(6*k)+0] = io_res_payload_0_0;
+    res[(6*k)+1] = io_res_payload_0_1;
+    res[(6*k)+2] = io_res_payload_0_2;
+    res[(6*k)+3] = io_res_payload_1_0;
+    res[(6*k)+4] = io_res_payload_1_1;
+    res[(6*k)+5] = io_res_payload_1_2;
+  end
+
+  $writememh("./tb/res.mem", res);
+  $finish;
+end
+
+always #5 clk = ! clk ;
+
+
+endmodule
---
+++ sim/tensor_core_array_tb_start.do
@@ -0,0 +1,33 @@
+set QSYS_SIMDIR .
+source $QSYS_SIMDIR/mentor/msim_setup.tcl
+
+dev_com
+com
+vlog ./tb/TensorCoreArrayTb.sv ../src/generated/TensorCoreChainArray.v
+set TOP_LEVEL_NAME TensorCoreArrayTb
+
+#vlib work
+#vmap work work
+
+#vcom is used for compile VHDL files
+
+
+# vlog is used for compile Verilog files
+vlog -reportprogress 300 -work work ./tb/fourteennm_atoms.sv
+
+
+elab_debug
+
+view signals
+view structure
+view wave
+
+# do tcchain_tb_add_wave.do
+
+log -r *
+
+
+
+
+
+
